4c2c9ed81fe5b2507e07158fcc5997ee7a65a05a
조금더하이얀 L <lapisraba2@naver.com>
2025-03-15 01:25:48 +0900

    초기 커밋 - sleepface

---
+++ Source/Sleepface/ConditionSleeping/ConditionSleeping.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml;
+using Verse;
+using AlienRace.ExtendedGraphics;
+using RimWorld;
+
+
+// ConditionSleeping 클래스 작성
+public class ConditionSleeping : Condition
+{
+    public new const string XmlNameParseKey = "Sleeping";
+
+    public bool sleeping = true;
+
+    public override bool Satisfied(ExtendedGraphicsPawnWrapper pawn, ref ResolveData data)
+    {
+        return this.sleeping == !pawn.WrappedPawn.Awake();
+    }
+}
